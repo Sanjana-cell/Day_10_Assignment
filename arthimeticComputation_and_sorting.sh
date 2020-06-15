d85a789ba56d46acebd65e07ce6c4b168cbe9273
Sanjana K R <sanjanakr97@gmail.com>
2020-06-15 20:36:36 +0530

    Added Code to store results in a dictionary for every computation

--- arthimeticComputation_and_sorting.sh
+++ arthimeticComputation_and_sorting.sh
@@ -10,4 +10,9 @@ echo "Result of (a+b*c)=" $result_1
 echo "Result of (a*b+c)=" $result_2
 echo "Result of (c+a/b)=" $result_3
 echo "Result of (a%b+c)=" $result_4
-
+declare -i result_dict
+result_dict[1]=$result_1
+result_dict[2]=$result_2
+result_dict[3]=$result_3
+result_dict[4]=$result_4
+echo ${result_dict[@]}
