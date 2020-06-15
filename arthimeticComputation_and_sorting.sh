023431ca66c93579b27f4721755e3ff4b0421ecc
Sanjana K R <sanjanakr97@gmail.com>
2020-06-15 11:56:37 +0530

    Added Code to Compute (c*a/b) equation

--- arthimeticComputation_and_sorting.sh
+++ arthimeticComputation_and_sorting.sh
@@ -4,6 +4,8 @@ read -p "Enter the value of b " b
 read -p "Enter the value of c " c
 result_1=$((($a+$b)*$c))
 result_2=$((($a*$b)+$c))
+result_3=$((($c+$a)/$b))
 echo "Result of (a+b*c)=" $result_1
 echo "Result of (a*b+c)=" $result_2
+echo "Result of (c+a/b)=" $result_3
 
