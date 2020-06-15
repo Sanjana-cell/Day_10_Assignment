384fa42eda7306d953c531834ffe3c28d64676cb
Sanjana K R <sanjanakr97@gmail.com>
2020-06-15 22:53:27 +0530

    Added Code to Sort the results to show the Computation Result in the Descending Order

--- arthimeticComputation_and_sorting.sh
+++ arthimeticComputation_and_sorting.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 read -p "Enter the value of a " a
 read -p "Enter the value of b " b
 read -p "Enter the value of c " c
@@ -15,4 +15,23 @@ result_dict[1]=$result_1
 result_dict[2]=$result_2
 result_dict[3]=$result_3
 result_dict[4]=$result_4
-echo ${result_dict[@]}
+for(( i=0; i<4; i++ ))
+do
+	t=$(($i+1))
+	arr[i]=${result_dict[t]}
+done
+echo ${arr[@]}
+for (( i=0; i<4; i++ ))
+do
+	for(( j=i+1; j<4; j++ ))
+	do
+		if [ ${arr[i]} -lt ${arr[j]} ]
+		then
+			temp=${arr[i]}
+			arr[i]=${arr[j]}
+			arr[j]=$temp
+		fi
+	done
+done
+echo "The array arranged in descending order are given below"
+echo ${arr[@]}
