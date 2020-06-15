1845c410e70295da2eab8d8b05a6bbcd5191a698
Sanjana K R <sanjanakr97@gmail.com>
2020-06-15 11:45:09 +0530

    Added Code to Compute (a*b+c) equation

--- arthimeticComputation_and_sorting.sh
+++ arthimeticComputation_and_sorting.sh
@@ -3,4 +3,7 @@ read -p "Enter the value of a " a
 read -p "Enter the value of b " b
 read -p "Enter the value of c " c
 result_1=$((($a+$b)*$c))
+result_2=$((($a*$b)+$c))
 echo "Result of (a+b*c)=" $result_1
+echo "Result of (a*b+c)=" $result_2
+
